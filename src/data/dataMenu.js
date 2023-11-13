9d3985e554fd7f0345e17ddff75130a9e9a7db26
jonn <joliversalamanca@gmail.com>
2023-11-13 11:37:15 -0400

    modified: HeaderMenu.astro, dataMenu.js

--- src/data/dataMenu.js
+++ src/data/dataMenu.js
@@ -2,5 +2,17 @@ export const dataMenu = [
     {
         name:'Como aprender ?',
         url:'#como-aprender'
-    }
+    },
+    {
+        name:'Porque aprender ingles ?',
+        url:'#porque-aprender-ingles'
+    },
+    {
+        name:'Motivate !',
+        url:'#motivacion'
+    },
+    {
+        name:'Cursos de ingles',
+        url:'#cursos-de-ingles'
+    },
 ]
